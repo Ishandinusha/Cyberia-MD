9b26157dcee8e64221615eecb4425144c13fdf8f
Ishandinusha <ishandinusha66@gmail.com>
2025-08-06 04:05:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with 
+  SESSION_ID: "drakonis~FiVUjYSD#R1jpeCtju1NhvrVWwaFCfqRKAHl32BOl-7Ls_ZzGa2Q",  // add your Session Id make sure it starts with 
   OWNER_NUMBER: "1(236)362-1958", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
